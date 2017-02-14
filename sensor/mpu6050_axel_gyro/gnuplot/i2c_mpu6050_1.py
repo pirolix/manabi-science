629bfee4aff3db522f4d2fc7cf2f692702c248cb
Piroli YUKARINOMIYA <piroli.yukarinomiya@gmail.com>
2017-02-14 18:34:19 +0900

    * first commit for "センサの測定値をグラフ描画"

--- sensor/mpu6050_axel_gyro/gnuplot/i2c_mpu6050_1.py
+++ sensor/mpu6050_axel_gyro/gnuplot/i2c_mpu6050_1.py
@@ -3,6 +3,7 @@
 import smbus            # use I2C
 import math
 from time import sleep  # time module
+import sys
 
 ### define #############################################################
 DEV_ADDR = 0x68         # device address
@@ -10,10 +11,6 @@ PWR_MGMT_1 = 0x6b       # Power Management 1
 ACCEL_XOUT = 0x3b       # Axel X-axis
 ACCEL_YOUT = 0x3d       # Axel Y-axis
 ACCEL_ZOUT = 0x3f       # Axel Z-axis
-TEMP_OUT = 0x41         # Temperature
-GYRO_XOUT = 0x43        # Gyro X-axis
-GYRO_YOUT = 0x45        # Gyro Y-axis
-GYRO_ZOUT = 0x47        # Gyro Z-axis
 
 # 1byte read
 def read_byte( addr ):
@@ -33,27 +30,6 @@ def read_word_sensor( addr ):
     else:
         return val - 65536 # negative value
 
-# Get Temperature
-def get_temp():
-    temp = read_word_sensor( TEMP_OUT )
-    # offset = -521 @ 35℃
-    return ( temp + 521 ) / 340.0 + 35.0
-
-# Get Gyro data (raw value)
-def get_gyro_data_lsb():
-    x = read_word_sensor( GYRO_XOUT )
-    y = read_word_sensor( GYRO_YOUT )
-    z = read_word_sensor( GYRO_ZOUT )
-    return [ x, y, z ]
-# Get Gyro data (deg/s)
-def get_gyro_data_deg():
-    x,y,z = get_gyro_data_lsb()
-    # Sensitivity = 131 LSB/(deg/s), @cf datasheet
-    x = x / 131.0
-    y = y / 131.0
-    z = z / 131.0
-    return [ x, y, z ]
-
 # Get Axel data (raw value)
 def get_accel_data_lsb():
     x = read_word_sensor( ACCEL_XOUT )
@@ -72,18 +48,13 @@ def get_accel_data_g():
 ### Main function ######################################################
 bus = smbus.SMBus( 1 )
 bus.write_byte_data( DEV_ADDR, PWR_MGMT_1, 0 )
+
+i = 1
 while 1:
-    temp = get_temp()
-    print 't= %.2f' % temp, '\t',
-
-    gyro_x,gyro_y,gyro_z = get_gyro_data_deg()
-    print 'Gx= %.3f' % gyro_x, '\t',
-    print 'Gy= %.3f' % gyro_y, '\t',
-    print 'Gz= %.3f' % gyro_z, '\t',
-
-    accel_x,accel_y,accel_z = get_accel_data_g()
-    print 'Ax= %.3f' % accel_x, '\t',
-    print 'Ay= %.3f' % accel_y, '\t',
-    print 'Az= %.3f' % accel_z, '\t',
-    print # 改行
-    sleep( 1 )
+    # 加速度
+    accel_x, accel_y, accel_z = get_accel_data_g()
+    # 小数点以下第3位まで表示.
+    print '%d %.3f %.3f %.3f' % ( i, accel_x, accel_y, accel_z )
+    i = i + 1
+    sys.stdout.flush()
+    sleep(0.1)
